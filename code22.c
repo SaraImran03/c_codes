dbd3742d76200986fdf8f7c6c1415f7fe0869578
SaraImran03 <saraimran2003@gmail.com>
2024-08-16 16:02:33 -0400

    Create code22.c

---
+++ code22.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  // Include the Standard Input Output library for the printf function
+#include <stdlib.h> // Include the Standard Library for general purposes functions
+
+int main() {
+    printf("Hello, World!\n"); // Use the printf function from stdio.h
+    return 0;
+}
